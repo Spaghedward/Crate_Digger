ff3035f586a349be88d154c9f36d3dafc36fc043
kmb8043 <you@example.com>
2023-12-04 22:20:13 -0500

    deleted the kims folder

--- kims/kims/footer-js/footer-javascript/script.js
+++
@@ -1,9 +0,0 @@
-
-var userEmail = document.querySelector('user-email');
-
-document.getElementById('email-btn').addEventListener('click' , function() {
-    console.log(userEmail.value)
-    alert("Welcome!");
-    userEmail.reset();
-});
-
